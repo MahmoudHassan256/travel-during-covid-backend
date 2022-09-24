d11cf39198f46661aae0998fdf8a73a37640d3b8
MrHazrdY <103119019+MrHazrdY@users.noreply.github.com>
2022-09-24 14:09:49 +0300

    a

--- dist/db/index.js
+++ dist/db/index.js
@@ -6,6 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.connectDb = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
 const connectDb = async () => {
-    await mongoose_1.default.connect("mongodb://localhost/FlightsProject");
+    await mongoose_1.default.connect("mongodb+srv://Mahmoud256:Mahmoud123@cluster0.16dwaqm.mongodb.net/Covid?retryWrites=true&w=majority");
 };
 exports.connectDb = connectDb;
