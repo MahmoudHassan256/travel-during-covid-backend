ba546f28b857c5ea7c0293edb1aea3237dd545d6
MrHazrdY <103119019+MrHazrdY@users.noreply.github.com>
2022-09-24 14:08:04 +0300

    a

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDb = async () => {
-  await mongoose.connect("mongodb://localhost/FlightsProject");
+  await mongoose.connect("mongodb+srv://Mahmoud256:Mahmoud123@cluster0.16dwaqm.mongodb.net/Covid?retryWrites=true&w=majority");
 };
 
 export { connectDb }
